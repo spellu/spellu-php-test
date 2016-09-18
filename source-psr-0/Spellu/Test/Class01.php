b581d296a73380919897b5e67a1b12ee65ba41d8
Fumio Furukawa <fumio@jumilla.me>
2016-09-18 20:43:54 +0900

    no message

--- source-psr-0/Spellu/Test/Class01.php
+++ source-psr-0/Spellu/Test/Class01.php
@@ -4,5 +4,41 @@ namespace Spellu\Test;
 
 class Class01
 {
+	const publicConstant = 'apple';
 
+	public static function staticPublicMethod()
+	{
+	}
+
+	protected static function staticProtectedMethod()
+	{
+	}
+
+	private static function staticPrivateMethod()
+	{
+	}
+
+	public function instancePublicMethod()
+	{
+	}
+
+	protected function instanceProtectedMethod()
+	{
+	}
+
+	private function instancePrivateMethod()
+	{
+	}
+
+	public static $staticPublicProperty = '';
+
+	protected static $staticProtectedProperty = '';
+
+	protected static $staticPrivateProperty = '';
+
+	public $instancePublicProperty = '';
+
+	protected $instanceProtectedProperty = '';
+
+	protected $instancePrivateProperty = '';
 }
